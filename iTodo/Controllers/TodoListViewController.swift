4159e6aa66fc0cf8b89bbcb9123bf5f6364c0001
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-03-10 20:54:11 -0800

    bootstraping iTodo App

---
+++ iTodo/Controllers/TodoListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TodoListViewController.swift
+//  iTodo
+//
+//  Created by Tabassum Tamanna on 3/10/21.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    @IBOutlet weak var taskTitle: UITextField!
+    @IBOutlet weak var addTaskButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
