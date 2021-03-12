3ea3a12c6757a436e7b382c245440c793405f17c
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-03-11 21:38:40 -0800

    Add Task and show in the table view
    New Feature:
    1. text field and Add button functionality add
    2. When add a task show it on the table view

--- iTodo/Controllers/TodoListViewController.swift
+++ iTodo/Controllers/TodoListViewController.swift
@@ -6,17 +6,73 @@
 //
 
 import UIKit
+import CoreData
 
-class TodoListViewController: UIViewController {
+class TodoListViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var taskTitle: UITextField!
     @IBOutlet weak var addTaskButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+   
+    var taskList : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        //disableAddTaskButton(true)
+        
+        self.tableView.delegate = self
+        self.taskTitle.delegate = self
+        self.taskTitle.text = ""
     }
     
+    @IBAction func addTaskTapped(_ sender: Any) {
+        addTask()
+    }
+    
+    func addTask() {
+        if self.taskTitle.text == "" {
+            print("Please enter a task")
+        }
+        
+        self.taskList.append(self.taskTitle.text!)
+        self.tableView.reloadData()
+        self.taskTitle.text = ""
+    }
+    
+    func disableAddTaskButton(_ isEnable : Bool) {
+        self.addTaskButton.isEnabled = !isEnable
+    }
+    
+    
+    // MARK: - Text Field Should Return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        addTask()
+    }
+}
 
+extension TodoListViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return taskList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskListViewCell")!
+       
+        let task = taskList[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel?.text = task
+        
+        return cell
+    }
+    
 }
