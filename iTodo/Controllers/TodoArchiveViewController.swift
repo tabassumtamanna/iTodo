1c2aa3b87b24a74c5c19e3c5a35525d00004172e
Tabassum Tamanna <tabassumtamanna2001@gmail.com>
2021-04-02 15:36:23 -0700

    Archive list shown by date group

--- iTodo/Controllers/TodoArchiveViewController.swift
+++ iTodo/Controllers/TodoArchiveViewController.swift
@@ -17,9 +17,15 @@ class TodoArchiveViewController: UIViewController,  UITableViewDataSource, UITab
     // MARK: - Variables
     var dataController: DataController!
     var fetchResultsController: NSFetchedResultsController<Task>!
+    var sections = [MonthSection]()
     
+    // MARK: - struct: Month Section
+    struct MonthSection{
+        var date: Date
+        var taskList : [Task]
+    }
     
-    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,8 +33,22 @@ class TodoArchiveViewController: UIViewController,  UITableViewDataSource, UITab
         self.archiveTableView.dataSource = self
         
         setupFetchResultsController()
+        
+        let groups = Dictionary(grouping: self.fetchResultsController.fetchedObjects!) { (fetchedObject) in
+            return firstDay(date: fetchedObject.createdDate!)
+        }
+        
+        self.sections = groups.map(MonthSection.init(date:taskList:))
+        self.sections.sort { (lhs, rhs) in lhs.date > rhs.date}
+        print(self.sections)
     }
     
+    // MARK: - First Day
+    fileprivate func firstDay(date: Date) ->  Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: date)
+        return calendar.date(from: components)!
+    }
     
     // MARK: - Setup Fetch Results Controller
     fileprivate func setupFetchResultsController(){
@@ -39,9 +59,6 @@ class TodoArchiveViewController: UIViewController,  UITableViewDataSource, UITab
         let sortDescriptorStatus = NSSortDescriptor(key: "status", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptorDate, sortDescriptorStatus]
         
-        //fetchRequest.propertiesToGroupBy = ["createdDate", "status"]
-        //fetchRequest.propertiesToFetch = ["createdDate", "status"]
-    
         
         let today = NSDate()
        
@@ -58,39 +75,41 @@ class TodoArchiveViewController: UIViewController,  UITableViewDataSource, UITab
             fatalError("The fetch could not be performed: \(error.localizedDescription)")
         }
         
-        print(fetchResultsController.fetchedObjects)
-        
     }
 
     // MARK: - Table view data source
 
+    // MARK: - Number Of Sections
    func numberOfSections(in tableView: UITableView) -> Int {
         
-        if let sections = fetchResultsController.sections{
-            print("section count: \(sections.count)")
-            return sections.count
-        }
-        return 0
+        return self.sections.count
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        if let sections = fetchResultsController.sections {
-            let currentSection = sections[section]
-            
-            return currentSection.numberOfObjects
-        }
-        
-        return  0
-    }
-
     
+    // MARK: - Title For Header In Section
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = self.sections[section]
+        let date = section.date
+        
+        let formatingDate = getFormattedDate(date: date, format: "MMM dd, yyyy")
+        
+        return formatingDate
+    }
+    
+    // MARK: - Number Of Rows In Section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = self.sections[section]
+        
+        return  section.taskList.count
+    }
+
+    // MARK: - Cell For Row At
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskArchiveViewCell", for: indexPath)
        
         
-        let task = fetchResultsController.object(at: indexPath)
-        
+        //let task = fetchResultsController.object(at: indexPath)
+        let section = self.sections[indexPath.section]
+        let task = section.taskList[indexPath.row]
         
         cell.textLabel?.text = task.taskTitle
         cell.textLabel?.textColor =  task.status ? .gray : .black
@@ -108,16 +127,7 @@ class TodoArchiveViewController: UIViewController,  UITableViewDataSource, UITab
         return cell
     }
     
-    /*
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if let sections = fetchResultsController.sections {
-            let currentSection = sections[section]
-            
-            return currentSection.name
-        }
-        return nil
-    }
-    */
+    // MARK: - Get Formatted Date
     func getFormattedDate(date: Date, format: String) -> String {
             let dateformat = DateFormatter()
             dateformat.dateFormat = format
